528054a8a9d8ea890bfafe7f6090491911c5807e
ShubhamFareye1 <shubham.patidar1@getfareye.com>
2022-11-24 22:20:46 +0530

    new user bug fix

--- src/api/Axios.js
+++ src/api/Axios.js
@@ -2,10 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
   withCredentials: true,
-<<<<<<< HEAD
-  baseURL: 'https://4b73-182-156-218-98.in.ngrok.io',
-  // baseURL:'http://10.0.2.2/',
-=======
   baseURL: 'https://4b73-182-156-218-98.in.ngrok.io/',
->>>>>>> f50ad7558f72cd9c20223aead640280cc4c3883c
 });
